617f41363c2208e71f0a4b43b645e25a8f58c397
Nathan Rymarz <nrymarz@gmail.com>
2020-10-10 01:44:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ function placeOrder(cardNumber='') {
     return "Sorry, we don't have a credit card on file for you."
   }
   else{
-    total = total();
+    totalCost = total();
     cart = [];
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
   }
 }
 function price(){
