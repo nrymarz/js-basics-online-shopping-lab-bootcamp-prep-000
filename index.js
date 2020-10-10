b148853ad906ff7f2784339b9395383cfa64d91d
Nathan Rymarz <nrymarz@gmail.com>
2020-10-10 01:32:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,7 +44,9 @@ function removeFromCart(item) {
   for(let i = 0;i < cart.length; i++){
     if (item === cart[i].itemName){
       cart.splice(i,1);
+      return 0;
     }
+    return "That item is not in your cart.";
   }
 }
 
