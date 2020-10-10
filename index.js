7106b78e83ef0fa0b799ab9bafbab6a558ee2839
Nathan Rymarz <nrymarz@gmail.com>
2020-10-10 01:32:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ function removeFromCart(item) {
       cart.splice(i,1);
       return 0;
     }
-    return "That item is not in your cart.";
   }
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
